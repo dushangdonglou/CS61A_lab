c6e231cb231264fe954766c8ff0a2a9814340c76
dushangdonglou <zhu.z.q@qq.com>
2022-07-12 12:26:48 +0800

    2022-7-12

--- lab/lab06/lab06.py
+++ lab/lab06/lab06.py
@@ -13,9 +13,56 @@ def insert_items(lst, entry, elem):
     >>> large_lst3
     [1, 4, 6, 4, 6, 8]
     >>> large_lst3 is large_lst
-    True
+    False
     """
     "*** YOUR CODE HERE ***"
+    # 方法1 建立new_lst
+    # new_lst = []
+    # for i in lst:
+    #     new_lst.append(i)
+    #     if i == entry:
+    #         new_lst.append(elem)
+    # return new_lst
+    # 方法2 在原lst上插入
+    # def insert(x, item):
+    #     nonlocal lst
+    #     lst1 = lst[:x]
+    #     lst2 = lst[x:]
+    #     lst = lst1 + [item] + lst2
+    # i = 0
+    # for _ in range(len(lst)):
+    #     if lst[i] == entry:
+    #         insert(i+1, elem)
+    #         i += 1
+    #     i += 1
+    # return lst
+
+    # 方法3 双指针
+    def insert(x, item):
+        nonlocal lst
+        lst1 = lst[:x]
+        lst2 = lst[x:]
+        lst = lst1 + [item] + lst2
+
+    left = 0
+    right = len(lst) - 1
+    while left <= right:
+        if left != right:
+            if lst[left] == entry:
+                insert(left + 1, elem)
+                right += 1
+                left += 1
+            if lst[right] == entry:
+                insert(right + 1, elem)
+                right -= 1
+            left += 1
+            right -= 1
+        else:
+            if lst[left] == entry:
+                insert(left + 1, elem)
+            break
+    return lst
+
 
 
 def naturals():
@@ -48,13 +95,14 @@ def scale(it, multiplier):
     [2, 4, 6, 8, 10]
     """
     "*** YOUR CODE HERE ***"
-
+    for i in it:
+        yield i * multiplier
 
 def hailstone(n):
     """Yields the elements of the hailstone sequence starting at n.
     
-    >>> for num in hailstone(10):
-    ...     print(num)
+    # >>> for num in hailstone(10):
+    # ...     print(num)
     ...
     10
     5
@@ -65,3 +113,8 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    if n <= 1:
+        if n % 2 == 0:
+            yield n / 2
+        else:
+            yield n * 3 + 1
